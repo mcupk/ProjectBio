6ce8eb6221a5072a55686ef704b58cc10bc263b7
mcupk <72302045+mcupk@users.noreply.github.com>
2020-12-20 18:43:20 +0700

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -31,9 +31,10 @@ FLAGS = flags.FLAGS
 def class_text_to_int(row_label):
         if row_label == 'lumbar scoliosis':
             return 1
+        elif row_label == 'not lumbar scoliosis':
+            return 2
         else:
            return None
-
 
 def split(df, group):
     data = namedtuple('data', ['filename', 'object'])
